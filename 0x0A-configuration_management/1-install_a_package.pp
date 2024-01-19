486336967dce8cab196f105b4dea67af9f9a482b
SCCSMARTCODE <sccsmart247@gmail.com>
2024-01-19 08:27:23 -0800

    pup

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 
 package { 'flask':
   ensure          => '2.1.0',
-  provider        => 'pip3',
-  name            => 'flask',
+  provider        => 'pip',
+  install_options => ['--user', '--force-reinstall'],
 }
