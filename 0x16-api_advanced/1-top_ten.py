343d310ed6580f4d505ddf16e8c08996b3c91dca
EMMANUEL AYOBAMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-03-06 10:35:45 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""
-prints the titles of the first 10 hot posts listed for a given subreddit
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        
-        if not posts:
-            print("No posts found in the subreddit.")
-        else:
-            for post in posts:
-                title = post.get("data", {}).get("title")
-                print(title)
-    else:
-        print("None")
+
+if __name__ == "__main__":
+    top_ten(argv[1])
